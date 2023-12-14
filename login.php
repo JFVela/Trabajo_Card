cee182477ac19395b616b90f500dc9e538290adb
JFVela <jfvela_2012@hotmail.com>
2023-12-14 10:37:29 -0500

    Update login.php

--- login.php
+++ login.php
@@ -12,14 +12,13 @@ if (isset($_SESSION['error_message'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>INICIAR SECCION</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/login.css">
 </head>
 
-<body>
-
-    <section class="vh-100 gradient-custom">
+<body class="vh-100 gradient-custom">
+    <section>
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <!--BOTONES-->
@@ -44,17 +43,7 @@ if (isset($_SESSION['error_message'])) {
                                         <input name="password" id="typePasswordX" class="form-control form-control-lg" />
                                         <label class="form-label" for="typePasswordX">Password</label>
                                     </div>
-
-                                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-
                                     <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-
-                                    <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                                        <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                                        <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                                        <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-                                    </div>
-
                                 </div>
                             </form>
                             <div>
